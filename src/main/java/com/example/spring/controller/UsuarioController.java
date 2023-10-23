6faa4e90f57f300fa3f46650ea8c37cfc1f7ada1
Lucas <lucas.confessor10@gmail.com>
2023-10-23 16:05:57 -0300

    editar fazendo request mas falta salvar

--- src/main/java/com/example/spring/controller/UsuarioController.java
+++ src/main/java/com/example/spring/controller/UsuarioController.java
@@ -2,6 +2,7 @@ package com.example.spring.controller;
 
 import com.example.spring.model.Usuario;
 import com.example.spring.repository.UsuarioRepository;
+import com.example.spring.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
@@ -9,16 +10,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
 import java.util.Optional;
 
 
-@Controller
+@RestController
 @Log4j2
+//@RequestMapping("usuario")
 @RequiredArgsConstructor
 public class UsuarioController {
 
 
     private final UsuarioRepository repository;
+
 
     @GetMapping("/")
     public String home() {
@@ -35,32 +39,34 @@ public class UsuarioController {
     @GetMapping("/lista")
     public ModelAndView lista() {
         ModelAndView mav = new ModelAndView("lista");
-        Iterable<Usuario> usuarioIterable = repository.findAll();
-        mav.addObject("lista", usuarioIterable);
+        List<Usuario> usuarios = repository.findAll();
+        mav.addObject("lista", usuarios);
         return mav;
 
     }
+
     @GetMapping("/deletar/{id}")
     public String deletar(@PathVariable Integer id) {
         repository.deleteById(id);
         return "redirect:/lista";
     }
+
     @GetMapping("editar/{id}")
-    public String editar(@PathVariable Integer id){
-        Optional<Usuario> usuarioOptional = repository.findById(id);
-        if (usuarioOptional.isPresent()) {
-            Usuario usuario = usuarioOptional.get();
-            repository.save(usuario);
-        } else {
-            // Trate o caso em que o usuário não foi encontrado com o id fornecido.
-        }
-        return "editar";
-    }
-    @PostMapping("editar/{id}")
-    public String editarUsuario(@PathVariable Integer id, Usuario usuario){
-        repository.save(usuario);
-        return "redirect: lista";
+    public ModelAndView editar(@PathVariable Integer id) {
+        ModelAndView mav = new ModelAndView("editar");
+        Optional<Usuario> usuarioVelho = repository.findById(id);
+        Usuario usuario = usuarioVelho.get();
+        mav.addObject("usuario", usuario);
+        return mav;
+
     }
 
+    @PostMapping("editar")
+    public String editar(Usuario usuarioAtualizado) {
+        repository.save(usuarioAtualizado);
+        return "redirect:/lista";
+    }
 }
+
+
 
